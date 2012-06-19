6a1df5bbce20beafed7c390ea540e072359443d0
Kristian Stangeland <kr_stang@hotmail.com>
2012-06-19 22:15:17 +0200

    Switched to join (sucessfully joined the server) instead of login (pending login).

--- ExperienceMod/src/com/comphenix/xp/ExperienceInformer.java
+++ ExperienceMod/src/com/comphenix/xp/ExperienceInformer.java
@@ -9,7 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class ExperienceInformer implements Listener {
 
@@ -19,7 +19,7 @@ public class ExperienceInformer implements Listener {
 	private List<String> warningMessages = new ArrayList<String>();
 
 	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-	public void onBlockBreakEvent(PlayerLoginEvent event) {
+	public void onPlayerJoinEvent(PlayerJoinEvent event) {
 		
 		Player player = event.getPlayer();
 		
