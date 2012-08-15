bbc5c941f94b31a7f4b8d050ede2aa9b112bdff3
Kristian Stangeland <kr_stang@hotmail.com>
2012-08-15 15:25:51 +0200

    Ignore NULL in processClickEvent.
    User Joymo reported of a NPE in StandardBlockService.processClickEvent
    (ticket #4) on line 48, which, given the dumped values, occurred because
    block was NULL somehow. 
    
    While I was unable to reproduce the problem, the conditions for it to
    occur (lag?) might be rare and a bigger problem than failing to handle
    custom blocks (only for Tekkit though). So, we'll simply ignore the NULL
    value when we get it - it's not really a problem since Bukkit has
    already done most of our work for us and given the correct inventory
    type. We only really need it for Tekkit custom block providers, which
    should already handle the NULL case.

--- ExperienceMod/src/com/comphenix/xp/mods/StandardBlockService.java
+++ ExperienceMod/src/com/comphenix/xp/mods/StandardBlockService.java
@@ -34,32 +34,53 @@ public class StandardBlockService implements BlockService {
 		switch (type) {
 			case BREWING:
 				// Make sure this is a potion result slot
-				if (isPotionResult && block.match(Material.BREWING_STAND)) {
+				if (isPotionResult && match(block, Material.BREWING_STAND)) {
 					return new BlockResponse(type, ActionTypes.BREWING, Permissions.REWARDS_BREWING);
 				}
+				break;
 				
 			case CRAFTING:		
 				// Player crafting - meaning that the block query is irrelevant
 				if (isCraftResult) {
 					return new BlockResponse(type, ActionTypes.CRAFTING, Permissions.REWARDS_CRAFTING);
 				}
+				break;
 				
 			case WORKBENCH:
-				if (isCraftResult && block.match(Material.WORKBENCH)) {
+				if (isCraftResult && match(block, Material.WORKBENCH)) {
 					return new BlockResponse(type, ActionTypes.CRAFTING, Permissions.REWARDS_CRAFTING);
 				}
+				break;
 				
 			case FURNACE:
-				if (isCraftResult && 
-						(block.match(Material.FURNACE) ||
-						 block.match(Material.BURNING_FURNACE))) {
+				if (isCraftResult && match(block, Material.FURNACE, Material.BURNING_FURNACE)) {
 					return new BlockResponse(type, ActionTypes.SMELTING, Permissions.REWARDS_SMELTING);
 				}
+				break;
 				
 			default:
-				// Unable to process block
-				return BlockResponse.FAILURE;
+				// Invalid
+				break;
 		}
+		
+		// Unable to process block
+		return BlockResponse.FAILURE;
+	}
+	
+	// Check the given block
+	private boolean match(ItemQuery block, Material... materials) {
+		// If we don't have a block, we've failed to record the last interaction - but we'll still
+		// accept standard actions.
+		if (block == null)
+			return true;
+		
+		// Find a match
+		for (Material mat : materials) {
+			if (block.match(mat))
+				return true;
+		}
+		
+		return false;
 	}
 	
 	@Override
